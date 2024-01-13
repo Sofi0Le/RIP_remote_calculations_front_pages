e4c4a3fd5f9d63735cf8c358e6fbef39c947b2da
sofi_flin <sofi_flin@sofi.flin.book>
2024-01-13 14:07:43 +0300

    proxy fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
-   },
+    port: 3030,
+    proxy: {
+      '/api/operations/': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+      },
+    },
+  },
   plugins: [react()],
 })
